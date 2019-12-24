4581264ce76fb1d75d4a38a02ab96d4caa32da70
Vinod Selvin <vinod.selvin@switchme.in>
2019-12-24 20:01:50 +0530

    Combination generator code added

--- public_html/assets/js/pages/helper.js
+++ public_html/assets/js/pages/helper.js
@@ -43,3 +43,63 @@ function getQueryVariable(variable) {
   
   return "/";
 }
+
+function generateAndSaveImagesCombination(){
+    
+    var images = getImages();
+    
+    var tops = images.hasOwnProperty('Tops') && images.Tops && images.Tops.length > 0 ? getIndexes(images.Tops) : [];
+    var bottoms = images.hasOwnProperty('Bottoms') && images.Bottoms && images.Bottoms.length > 0 ? getIndexes(images.Bottoms) : [];
+    var footwears = images.hasOwnProperty('Footwear') && images.Footwear && images.Footwear.length > 0 ? getIndexes(images.Footwear) : [];
+
+    var images_ids = [tops, bottoms, footwears];
+    
+    var combos = allPossibleCombinations(images_ids);
+    
+    console.log(combos);
+}
+
+function getIndexes(arr){
+    
+    var indexes = [];
+    
+    for(var x in arr){
+        indexes.push(x);
+    }
+    
+    return indexes;
+}
+
+function allPossibleCombinations(items, isCombination=false){
+    // finding all possible combinations of the last 2 items
+    // remove those 2, add these combinations
+    // isCombination shows if the last element is itself part of the combination series
+    if(items.length == 1){
+       return items[0]
+    }
+    else if(items.length == 2){
+       var combinations = []
+       for (var i=0; i<items[1].length; i++){
+           for(var j=0; j<items[0].length; j++){
+               if(isCombination){
+                   // clone array to not modify original array
+                   var combination = items[1][i].slice();
+                   combination.push(items[0][j]);
+               }
+               else{
+                   var combination = [items[1][i], items[0][j]];
+               }
+               combinations.push(combination);
+           }
+       }
+       return combinations
+    }
+    else if(items.length > 2){
+       var last2 = items.slice(-2);
+       var butLast2 = items.slice(0, items.length - 2);
+       last2 = allPossibleCombinations(last2, isCombination);
+       butLast2.push(last2)
+       var combinations = butLast2;
+       return allPossibleCombinations(combinations, isCombination=true)
+    }
+}
